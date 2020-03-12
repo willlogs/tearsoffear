3ff6d697af53eb17e99f4a37f99d393b90c77c9e
theboywhomakesgames <m4ds31@gmail.com>
2020-03-12 12:35:10 +0330

    hauntedhouse in progress

---
+++ Assets/Scripts/Utils/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Quaternion baseRot;
+    public float rot = 90;
+
+    public AudioClip squeek;
+    public AudioSource source;
+
+    private void Start()
+    {
+
+    }
+
+    public void OpenUp()
+    {
+
+    }
+
+    public void Close()
+    {
+
+    }
+}
